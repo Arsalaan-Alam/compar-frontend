71a218b01f97fb10554b49431e686da8945997e9
saklani <50768838+saklani@users.noreply.github.com>
2023-12-06 01:32:37 +0530

    improve formatting

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,14 @@
 "use client";
-import { useRouter } from "next/navigation";
-import { useState, useEffect } from "react";
-import { ArConnect } from 'arweavekit/auth'
-import Dots from "./components/dots";
 import { Button } from "@/components/ui/button";
+import { ArConnect } from 'arweavekit/auth';
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
 
 export default function page() {
 
   const [user, setUser] = useState({ loggedIn: null });
   const router = useRouter();
-  
+
   const AuthedState = () => {
     return (
       <div>
@@ -17,17 +16,17 @@ export default function page() {
       </div>
     )
   }
-  
+
   const UnauthenticatedState = () => {
     return (
       <div>
-      <Button onClick={handleConnectWallet}>
-        Connect Wallet
-      </Button>
-      <Button onClick={handleLogin}>
-        Sign In with Email
-      </Button>
-    </div>
+        <Button onClick={handleConnectWallet}>
+          Connect Wallet
+        </Button>
+        <Button onClick={handleLogin}>
+          Sign In with Email
+        </Button>
+      </div>
     )
   }
   const handleConnectWallet = async () => {
@@ -77,15 +76,15 @@ export default function page() {
       <div className="max-w-7xl mx-auto px-4 sm:px-6">
         <div className="pt-32 pb-12 md:pt-40 md:pb-20">
           <div className="text-center pb-12 md:pb-16">
-          
-          <h1 className="text-8xl md:text-6xl font-extrabold leading-tighter tracking-tighter mb-4" data-aos="zoom-y-out">File Management on Arweave <span className="bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-teal-400">Made Easy.</span></h1>
+
+            <h1 className="text-8xl md:text-6xl font-extrabold leading-tighter tracking-tighter mb-4" data-aos="zoom-y-out">File Management on Arweave <span className="bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-teal-400">Made Easy.</span></h1>
             <div className="max-w-3xl mx-auto">
               <p className="text-xl text-gray-600 mb-8" data-aos="zoom-y-out" data-aos-delay="150">Compress your files easily with our decentralized compression service. Maintain access-control over your files. Save space and maintain the integrity of your data on Arweave.</p>
               <div className="max-w-xs mx-auto sm:max-w-none sm:flex sm:justify-center" data-aos="zoom-y-out" data-aos-delay="300">
-              {user.loggedIn ? <AuthedState /> : <UnauthenticatedState /> }
+                {user.loggedIn ? <AuthedState /> : <UnauthenticatedState />}
               </div>
             </div>
-          </div>         
+          </div>
         </div>
       </div>
     </section>
